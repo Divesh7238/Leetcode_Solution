eae9d7e949db8666a2024783d631ebee309add81
Divesh Singh <diveshsingh633@gmail.com>
2025-08-10 13:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (40.38%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -4,17 +4,17 @@ class Solution {
             // reverse Array
             int i = 0;
             int j = row.length -1;
-            while(i < j){
-                int temp = row[i];
-                row[i] = row[j];
+            while(i <= j){
+                int temp = row[i]^1;
+                row[i] = row[j]^1;
                 row[j] = temp;
                 i++;
                 j--;
             }
-            for(int k=0; k<row.length;k++){
-                row[k] = row[k] ^ 1;
+            // for(int k=0; k<row.length;k++){
+            //     row[k] = row[k] ^ 1;
 
-            }
+            // }
 
 
         }
